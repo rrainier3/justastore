529450ad2e84dafbfc4c25ec9a0a2199054733dc
Ray Rainier <rayrainier@yahoo.com>
2017-03-31 16:53:48 -0500

    re initialize

---
+++ Classes/DataProviders/TabSwitcherItemsProvider.swift
@@ -0,0 +1,90 @@
+//
+//  TabItemsProvider.swift
+//  ColorMatchTabs
+//
+//  Created by RayRainier on 2/24/17.
+//  Copyright © 2017 RadiuSense. All rights reserved.
+//
+
+import UIKit
+import ColorMatchTabs
+
+class TabItemsProvider {
+    
+    static let items = {
+        return [
+            TabItem(
+                title: "Grilled",
+                tintColor: UIColor.gray.withAlphaComponent(0.70),
+                //tintColor: UIColor(red: 0.51, green: 0.72, blue: 0.25, alpha: 1.00),
+                normalImage: UIImage(named: "Grill")!,
+                highlightedImage: UIImage(named: "GrillFilled")!
+            ),
+            TabItem(
+                title: "Veggies",
+                tintColor: UIColor.gray.withAlphaComponent(0.70),
+                //                tintColor: UIColor(red: 0.15, green: 0.67, blue: 0.99, alpha: 1.00),
+                normalImage: UIImage(named: "NaturalFood")!,
+                highlightedImage: UIImage(named: "NaturalFoodFilled")!
+            ),
+            TabItem(
+                title: "Noodles",
+                tintColor: UIColor.gray.withAlphaComponent(0.70),
+                //                tintColor: UIColor(red: 1.00, green: 0.61, blue: 0.16, alpha: 1.00),
+                normalImage: UIImage(named: "Noodles")!,
+                highlightedImage: UIImage(named: "NoodlesFilled")!
+            ),
+            TabItem(
+                title: "HotFood",
+                tintColor: UIColor.gray.withAlphaComponent(0.70),
+                //                tintColor: UIColor(red: 0.96, green: 0.61, blue: 0.58, alpha: 1.00),
+                normalImage: UIImage(named: "FryingPan")!,
+                highlightedImage: UIImage(named: "FryingPanFilled")!
+            )
+        ]
+    }()
+    
+}
+
+
+//class TabItemsProvider {
+//    
+//    static let items = {
+//        return [
+//            TabItem(
+//            	title: "",
+////                title: "Products",
+//				tintColor: UIColor.gray.withAlphaComponent(0.70),
+//                //tintColor: UIColor(red: 0.51, green: 0.72, blue: 0.25, alpha: 1.00),
+//                normalImage: UIImage(named: "products_normal")!,
+//                highlightedImage: UIImage(named: "products_highlighted")!
+//            ),
+//            TabItem(
+//            	title: "",
+////                title: "Places",
+//				tintColor: UIColor.gray.withAlphaComponent(0.70),
+////                tintColor: UIColor(red: 0.15, green: 0.67, blue: 0.99, alpha: 1.00),
+//                normalImage: UIImage(named: "venues_normal")!,
+//                highlightedImage: UIImage(named: "venues_highlighted")!
+//            ),
+//            TabItem(
+//            	title: "",
+////                title: "Reviews",
+//				tintColor: UIColor.gray.withAlphaComponent(0.70),
+////                tintColor: UIColor(red: 1.00, green: 0.61, blue: 0.16, alpha: 1.00),
+//                normalImage: UIImage(named: "reviews_normal")!,
+//                highlightedImage: UIImage(named: "reviews_highlighted")!
+//            ),
+//            TabItem(
+//            	title: "",
+////                title: "Friends",
+//				tintColor: UIColor.gray.withAlphaComponent(0.70),
+////                tintColor: UIColor(red: 0.96, green: 0.61, blue: 0.58, alpha: 1.00),
+//                normalImage: UIImage(named: "users_normal")!,
+//                highlightedImage: UIImage(named: "users_highlighted")!
+//            )
+//        ]
+//    }()
+//    
+//}
+
---
+++ Classes/ViewControllers/ExampleViewContoller.swift
@@ -0,0 +1,88 @@
+//
+//  ExampleViewContoller.swift
+//  ColorMatchTabs
+//
+//  Created by RayRainier on 2/24/17.
+//  Copyright © 2017 RadiuSense. All rights reserved.
+//
+
+import UIKit
+import ColorMatchTabs
+
+var refTintColor: UIColor = UIColor.clear
+
+class ExampleViewContoller: ColorMatchTabsViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        titleLabel.font = UIFont.navigationTitleFont()!
+        
+        //titleLabel.font = UIFont.fontAvenirMedium(ofSize: 24)
+        
+        // to hide bottom button comment the following line
+//        popoverViewController = ExamplePopoverViewController()
+//        popoverViewController?.delegate = self
+        
+        dataSource = self
+        reloadData()
+    }
+
+}
+
+extension ExampleViewContoller: ColorMatchTabsViewControllerDataSource {
+
+    
+    func numberOfItems(inController controller: ColorMatchTabsViewController) -> Int {
+        return TabItemsProvider.items.count
+    }
+    
+    func tabsViewController(_ controller: ColorMatchTabsViewController, viewControllerAt index: Int) -> UIViewController {
+        
+        return StubContentViewControllersProvider.viewControllers[index]
+    }
+    
+    func tabsViewController(_ controller: ColorMatchTabsViewController, titleAt index: Int) -> String {
+        
+        return TabItemsProvider.items[index].title
+    }
+    
+    func tabsViewController(_ controller: ColorMatchTabsViewController, iconAt index: Int) -> UIImage {
+    
+        return TabItemsProvider.items[index].normalImage
+    }
+    
+    func tabsViewController(_ controller: ColorMatchTabsViewController, hightlightedIconAt index: Int) -> UIImage {
+    
+        return TabItemsProvider.items[index].highlightedImage
+    }
+    
+    func tabsViewController(_ controller: ColorMatchTabsViewController, tintColorAt index: Int) -> UIColor {
+    
+        switch index {
+        
+            case 1:
+                titleLabel.text = "HONG-NING"
+            case 2:
+                titleLabel.text = "JO-FEN"
+            case 3:
+                titleLabel.text = "STARBUCKS"
+            default:
+                titleLabel.text = "WHOLEFOODS"
+            
+        }
+        
+        refTintColor = TabItemsProvider.items[index].tintColor
+        
+        return TabItemsProvider.items[index].tintColor
+    }
+    
+}
+
+extension ExampleViewContoller: PopoverViewControllerDelegate {
+
+    func popoverViewController(_ popoverViewController: PopoverViewController, didSelectItemAt index: Int) {
+        selectItem(at: index)
+    }
+    
+}
