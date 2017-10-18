e056aaad66dc8c16b0f3c9398bc09bb7a72f42c5
Ray Rainier <rayrainier@yahoo.com>
2017-10-18 16:26:17 -0500

    Update basket 1.1

--- justorderstore/BasketTableViewCell.swift
+++ justorderstore/BasketTableViewCell.swift
@@ -54,7 +54,7 @@ class BasketTableViewCell: UITableViewCell {
     let ProductImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFill
-        //imageView.image = UIImage(named: "product_card1")
+        imageView.image = UIImage(named: "koreanchicken")
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.clipsToBounds = true
         return imageView
